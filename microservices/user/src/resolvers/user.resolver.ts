fa5f74c5f8bd58cecce72994b7427b749e332183
Eric Stanley <eric.stanleyjones@gmail.com>
2025-02-27 23:37:21 +0530

    removed comments from user resolver

--- microservices/user/src/resolvers/user.resolver.ts
+++ microservices/user/src/resolvers/user.resolver.ts
@@ -10,24 +10,24 @@ import { HTTPExceptionFilter } from '../filters/exception.filter';
 import { Inject } from '@nestjs/common';
 import { GraphQLJSON } from 'graphql-type-json';
 
-@Resolver() // No specific type binding, since you're not using an entity
+@Resolver()
 @UseGuards(AuthGuard)
 @UseInterceptors(LoggingInterceptor)
 @UseFilters(HTTPExceptionFilter)
 export class UserResolver {
   constructor(@Inject('UserService') private readonly userService: UserServiceInterface) {}
 
-  @Mutation(() => CreateUserDto) // Adjust return type based on your service's actual returns.
+  @Mutation(() => CreateUserDto)
   async createUser(@Args('createUserDto') createUserDto: CreateUserDto): Promise<any> {
     return this.userService.createUser(createUserDto);
   }
 
-  @Query(() => String) // Adjust return type based on your service's actual returns.
+  @Query(() => String)
   async user(@Args('id', { type: () => ID }) id: string): Promise<any> {
     return this.userService.getUserById(id);
   }
 
-  @Mutation(() => String) // Adjust return type based on your service's actual returns.
+  @Mutation(() => String)
   async updateUser(
     @Args('id', { type: () => ID }) id: string,
     @Args('updateUserDto') updateUserDto: UpdateUserDto,
@@ -35,7 +35,7 @@ export class UserResolver {
     return this.userService.updateUser(id, updateUserDto);
   }
 
-  @Mutation(() => String) // Adjust return type based on your service's actual returns.
+  @Mutation(() => String)
   async deleteUser(@Args('id', { type: () => ID }) id: string): Promise<any> {
     return this.userService.deleteUser(id);
   }
